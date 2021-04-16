17a4213e5ed952f8171a211c3bd1b1d437447c1f
Quan Tran <tran.q90@gmail.com>
2021-04-15 20:30:02 -0700

    errormodal added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
 
+  const addUserHandler = (enteredUsername, enteredAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, {name: enteredUsername, age: enteredAge, id: Math.random().toString()}];
+    })
+  }
 
   return (
     <div>
-      <AddUser/>
-      <UsersList users={[]}/>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList}/>
     </div>
   );
 }
